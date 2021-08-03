5051c89e765cca830bb5520be09990137eb6530f
Zathras <ChampionMike42@gmail.com>
2021-08-03 14:40:10 -0500

    Add models/index.js

--- models/index.js
+++ models/index.js
@@ -1,20 +1,40 @@
 // import models
-const Product = require('./Product');
-const Category = require('./Category');
-const Tag = require('./Tag');
-const ProductTag = require('./ProductTag');
+const Product = require("./Product");
+const Category = require("./Category");
+const Tag = require("./Tag");
+const ProductTag = require("./ProductTag");
 
 // Products belongsTo Category
+Products.belongsTo(Category, {
+    foreignKey: "category_id",
+});
 
 // Categories have many Products
+Category.belongsToMany(Products, {
+    foreignKey: "product_id",
+});
 
 // Products belongToMany Tags (through ProductTag)
+Products.belongsToMany(Tags, {
+    through: {
+        model: ProductTag,
+        unique: false,
+    },
+    as: "product_tag",
+});
 
 // Tags belongToMany Products (through ProductTag)
+Tags.belongsToMany(Products, {
+    through: {
+        model: ProductTag,
+        unique: false,
+    },
+    as: "product_tag",
+});
 
 module.exports = {
-  Product,
-  Category,
-  Tag,
-  ProductTag,
+    Product,
+    Category,
+    Tag,
+    ProductTag,
 };
