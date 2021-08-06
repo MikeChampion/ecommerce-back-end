cdbbac1dff198153bba191212b1aa688b62b6005
Zathras <ChampionMike42@gmail.com>
2021-08-05 22:18:26 -0500

    minor fixes to Product model and index for joins

--- models/index.js
+++ models/index.js
@@ -11,14 +11,14 @@ Product.belongsTo(Category, {
 
 // Categories have many Products
 Category.hasMany(Product, {
-    foreignKey: "product_id",
+    foreignKey: "category_id",
 });
 
 // Products belongToMany Tags (through ProductTag)
 Product.belongsToMany(Tag, {
     through: {
         model: ProductTag,
-        unique: false,
+        foreignKey: "product_id",
     },
 });
 
@@ -26,7 +26,7 @@ Product.belongsToMany(Tag, {
 Tag.belongsToMany(Product, {
     through: {
         model: ProductTag,
-        unique: false,
+        foreignKey: "tag_id",
     },
 });
 
